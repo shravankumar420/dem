8ff229453e2f2a285b2c3e8640bc09066b2c60b6
shravan <s.v.shravankumar420@gmail.com>
2023-05-24 10:14:57 +0530

    Final code push

--- src/test/java/CucumberOptions/JunitTestRunnerTest.java
+++ src/test/java/CucumberOptions/JunitTestRunnerTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", dryRun = false, monochrome = true,
-		tags = "@SmokeTest or @Demo",
+		tags = "@SmokeTest",
 		plugin = { "pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cucumber.xml",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failed_scenarios.txt" })
